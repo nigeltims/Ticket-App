e84d2289e1752efcf56905d22ecb4d739df7b94f
KrishKrosh <krish.life21@gmail.com>
2020-04-07 14:30:18 -0600

    testing OCR

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -10,14 +10,12 @@ import 'models/ticketItem.dart';
 import 'screens/login_screen.dart';
 import 'services/authService.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-    
   String uid;
 
   File pickedImage;
@@ -32,28 +30,24 @@ class _HomePageState extends State<HomePage> {
     readText();
   }
 
-  Future readText() async{
+  Future readText() async {
     FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
     TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
     VisionText readText = await recognizeText.processImage(ourImage);
 
-    for (TextBlock block in readText.blocks){
-      for (TextLine line in block.lines){
-        for (TextElement word in line.elements){
-          print(word.text);
-        }
-      }
+    for (TextBlock block in readText.blocks) {
+      print(block.text);
     }
   }
 
   @override
-  void initState(){
+  void initState() {
     this.uid = '';
-    FirebaseAuth.instance.currentUser().then((val){
+    FirebaseAuth.instance.currentUser().then((val) {
       setState(() {
         this.uid = val.uid;
       });
-    }).catchError((e){
+    }).catchError((e) {
       print(e);
     });
 
@@ -63,132 +57,126 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-  floatingActionButton: FloatingActionButton(
-    backgroundColor: Color(0xffffffff),
-    foregroundColor: Color(0xff2BC8D8),
-    shape: CircleBorder(side: BorderSide(color: Color(0xff2BC8D8), width: 2.0)),
-    onPressed: pickImage,//() { },
-    tooltip: 'Increment',
-    child: Icon(Icons.add),
-    elevation: 2.0,
-  ),
-    bottomNavigationBar: BottomAppBar(
-      notchMargin: 8,
-      shape: CircularNotchedRectangle(),
-      clipBehavior: Clip.antiAlias,
-      child: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xffffffff),
+        foregroundColor: Color(0xff2BC8D8),
+        shape: CircleBorder(
+            side: BorderSide(color: Color(0xff2BC8D8), width: 2.0)),
+        onPressed: pickImage, //() { },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        elevation: 2.0,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        shape: CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              title: Text('Business'),
+            ),
+          ],
+          selectedItemColor: Color(0xff2BC8D8),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.business),
-          title: Text('Business'),
-        ),
-      ],
-      selectedItemColor: Color(0xff2BC8D8),
-
-    ),
-    ),
+      ),
       body: ListPage(),
 
+      // ListView(
+      //   physics: BouncingScrollPhysics(),
+      //   children: <Widget>[
+      //     SizedBox(
+      //       height: 20,
+      //     ),
+      //     Row(
+      //       children: <Widget>[
+      //       SizedBox(
+      //       width: 20,
+      //       ),
+      //         Text("Tickets",
+      //         style: TextStyle(
+      //           color: Color(0xff241A3C),
+      //           // fontFamily: 'FuturaBold',
+      //           fontSize: 30,
+      //           fontWeight: FontWeight.bold
+      //         ),
+      //         ),
+      //       SizedBox(
+      //       width: 180,
+      //       ),
+      //       IconButton(icon: Icon(Icons.settings), onPressed: (){
+      //             FirebaseAuth.instance.signOut();
+      //       Navigator.push(context,
+      //           CupertinoPageRoute(builder: (context) => AuthService().handleAuth()));
+      //       })
+      //       ],
+      //     ),
 
+      //     Column(
+      //       children: <Widget>[
+      //         SizedBox(
+      //           height: 15,
+      //         ),
+      //     TicketItem(
+      //       foreColor: Color(0xff2BC8D8),
+      //       backColor: Color(0xffE5F7F8),
+      //       plateID: "SUR 360",
+      //       reason: "Parking in front of fire hydrant",
+      //       amount: "\$55",
+      //       address: "153 Burry Road",
+      //       date: DateTime.now(),
+      //     ),
+      //         SizedBox(
+      //           height: 15,
+      //         ),
+      //     TicketItem(
+      //       foreColor: Color(0xffFF6E6E),
+      //       backColor: Color(0xffFFF1F1),
+      //       plateID: "SUR 360",
+      //       reason: "Parking in front of fire hydrant",
+      //       amount: "\$55",
+      //       address: "153 Burry Road",
+      //       date: DateTime.now(),
+      //     ),
+      //         SizedBox(
+      //           height: 15,
+      //         ),
+      //     TicketItem(
+      //       foreColor: Color(0xff2BC8D8),
+      //       backColor: Color(0xffE5F7F8),
+      //       plateID: "SUR 360",
+      //       reason: "Parking in front of fire hydrant",
+      //       amount: "\$55",
+      //       address: "153 Burry Road",
+      //       date: DateTime.now(),
+      //     ),
+      //         SizedBox(
+      //           height: 15,
+      //         ),
+      //     TicketItem(
+      //       foreColor: Color(0xffFF6E6E),
+      //       backColor: Color(0xffFFF1F1),
+      //       plateID: "SUR 360",
+      //       reason: "Parking in front of fire hydrant",
+      //       amount: "\$55",
+      //       address: "153 Burry Road",
+      //       date: DateTime.now(),
+      //     ),
 
-    // ListView(
-    //   physics: BouncingScrollPhysics(),
-    //   children: <Widget>[
-    //     SizedBox(
-    //       height: 20,
-    //     ),
-    //     Row(
-    //       children: <Widget>[
-    //       SizedBox(
-    //       width: 20,
-    //       ),
-    //         Text("Tickets",
-    //         style: TextStyle(
-    //           color: Color(0xff241A3C),
-    //           // fontFamily: 'FuturaBold',
-    //           fontSize: 30,
-    //           fontWeight: FontWeight.bold
-    //         ),
-    //         ),
-    //       SizedBox(
-    //       width: 180,
-    //       ),
-    //       IconButton(icon: Icon(Icons.settings), onPressed: (){
-    //             FirebaseAuth.instance.signOut();
-    //       Navigator.push(context,
-    //           CupertinoPageRoute(builder: (context) => AuthService().handleAuth()));
-    //       })
-    //       ],
-    //     ),
-        
-        
-    //     Column(
-    //       children: <Widget>[
-    //         SizedBox(
-    //           height: 15,
-    //         ),
-    //     TicketItem(
-    //       foreColor: Color(0xff2BC8D8),
-    //       backColor: Color(0xffE5F7F8),
-    //       plateID: "SUR 360",
-    //       reason: "Parking in front of fire hydrant",
-    //       amount: "\$55", 
-    //       address: "153 Burry Road",
-    //       date: DateTime.now(),
-    //     ),
-    //         SizedBox(
-    //           height: 15,
-    //         ),
-    //     TicketItem(
-    //       foreColor: Color(0xffFF6E6E),
-    //       backColor: Color(0xffFFF1F1),
-    //       plateID: "SUR 360",
-    //       reason: "Parking in front of fire hydrant",
-    //       amount: "\$55", 
-    //       address: "153 Burry Road",
-    //       date: DateTime.now(),
-    //     ),
-    //         SizedBox(
-    //           height: 15,
-    //         ),
-    //     TicketItem(
-    //       foreColor: Color(0xff2BC8D8),
-    //       backColor: Color(0xffE5F7F8),
-    //       plateID: "SUR 360",
-    //       reason: "Parking in front of fire hydrant",
-    //       amount: "\$55", 
-    //       address: "153 Burry Road",
-    //       date: DateTime.now(),
-    //     ),
-    //         SizedBox(
-    //           height: 15,
-    //         ),
-    //     TicketItem(
-    //       foreColor: Color(0xffFF6E6E),
-    //       backColor: Color(0xffFFF1F1),
-    //       plateID: "SUR 360",
-    //       reason: "Parking in front of fire hydrant",
-    //       amount: "\$55", 
-    //       address: "153 Burry Road",
-    //       date: DateTime.now(),
-    //     ),
+      //         SizedBox(
+      //           height: 35,
+      //         ),
+      //       ],
+      //     )
+      //   ],
 
-    //         SizedBox(
-    //           height: 35,
-    //         ),
-    //       ],
-    //     )
-    //   ],
-
-    // ),
-    
-
-        
+      // ),
     );
   }
 }
